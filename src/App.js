ed38c2a65c86adebf10e7d57cbdde2bbf03f1943
Emily Déat <emily.deat@gmail.com>
2020-03-31 14:31:18 +0200

    Add ContactList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
 import Contact from "./components/Contact";
+import ContactList from "./components/ContactList"
 
 const App = () => {
   return (
     <div className="App">
-      <Contact
-        avatar="https://randomuser.me/api/portraits/men/18.jpg"
-        name="Alvin Hunt"
-        online
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/men/23.jpg"
-        name="Tim Olivier"
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/38.jpg"
-        name="Anna Price"
-        online
-      />
+      <ContactList />
     </div>
   );
 };
